e1bba01c87d246bbd025a05fe9dbbea97a2bed83
Horacio Borrego <hborrego365@gmail.com>
2021-06-01 15:18:55 -0700

    Created Header component, import Header to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
       <main className="app-container">
-        <h1>hello</h1>
+        <Header />
       </main>
     </Router>
   );
