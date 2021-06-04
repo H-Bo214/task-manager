445ce14a189ae13cadc8e431cadfb2c5da0b15b6
Horacio Borrego <hborrego365@gmail.com>
2021-06-04 13:54:53 -0700

    create mock data file

---
+++ mockData.js
@@ -0,0 +1,18 @@
+export const mockData = [
+  {id: 1,
+  task:"Go to grocery store.", 
+  date:"June 1 2021, 9:00am", 
+  priority: false 
+  },
+  {id: 2,
+  task:"Pay bills", 
+  date:"June 4 2021, 10:00am", 
+  priority: false 
+  },
+  {id: 3,
+  task:"Doctor's appointment", 
+  date:"June 10 2021, 12:00pm", 
+  priority: true 
+  },
+]
+
