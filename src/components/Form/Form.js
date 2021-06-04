3b8d06001ab788d1d7184d2f625b56d6dfe8537b
Horacio Borrego <hborrego365@gmail.com>
2021-06-04 13:45:36 -0700

    Create error state if no task or date is chosen. Display error to user.

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,16 +7,24 @@ const Form = () => {
   const [text, setText] = useState('')
   const [startDate, setStartDate] = useState(null)
   const [priority, setPriority] = useState(false)
+  const [error, setError] = useState(null)
 
   const onSubmit = (e) => {
     e.preventDefault()
+    if (!text || !startDate) {
+      setError('⚠️ Please enter a task and date.')
+      return
+    }
+    //API call passed down from App.js will go here.
     setStartDate(null)
     setText('')
     setPriority(false)
+    setError(null)
   }
 
   return (
     <form >
+      {error && <p>{error}</p>}
       <label>Task</label>
       <input 
         type='text' 
@@ -26,7 +34,6 @@ const Form = () => {
       />
       <label>Date & Time</label>
       <DatePicker
-      
       selected={startDate}
       onChange={(date) => setStartDate(date)}
       shouldCloseOnSelect={false}
