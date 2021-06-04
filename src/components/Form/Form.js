730a4785a0d971c3e523657d24dc97aebfc2b99f
Horacio Borrego <hborrego365@gmail.com>
2021-06-04 13:18:33 -0700

    Add checkbox for priority task, create state for checkbox, reset state to default state once form submitted

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,12 +6,13 @@ import "react-datepicker/dist/react-datepicker.css"
 const Form = () => {
   const [text, setText] = useState('')
   const [startDate, setStartDate] = useState(null)
+  const [priority, setPriority] = useState(false)
 
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log('I was submitted.')
     setStartDate(null)
     setText('')
+    setPriority(false)
   }
 
   return (
@@ -19,7 +20,7 @@ const Form = () => {
       <label>Task</label>
       <input 
         type='text' 
-        placeholder="Add Task"
+        placeholder='Add Task'
         value ={text}
         onChange={(e) => setText(e.target.value)}
       />
@@ -32,7 +33,14 @@ const Form = () => {
       showTimeSelect
       dateFormat="MMMM d, yyyy, h:mm aa"
       placeholderText=" Select a date & time"
-    />  
+    />
+    <label>Priority task?</label>  
+    <input
+      type='checkbox'
+      checked={priority}
+      value={priority}
+      onChange={(e) => setPriority(e.target.checked)}
+    />
       <Button 
         btnText='Submit this task'
         onClick={onSubmit}
