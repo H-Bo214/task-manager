73072fe810f9f1101e5b61e752f95fa545ba53d2
Horacio Borrego <hborrego365@gmail.com>
2021-06-16 14:47:02 -0700

    add task tests

--- src/components/Task/Task.test.js
+++ src/components/Task/Task.test.js
@@ -32,12 +32,14 @@ describe('Task',  () => {
 
   it('should remove the task when the delete img is clicked',  () => {
     let deleteTask = jest.fn()
+
     let emptyTask = {
       "text": "",
-      "date": "2021-07-17T18:00:00.000Z",
+      "date": 'December 31, 1969, 4:00 PM',
       "priority": false,
       "id": 2
     }
+
     let task = {
       "text": "Dentist appointment",
       "date": "July 17, 2021, 11:00 AM",
@@ -62,8 +64,6 @@ describe('Task',  () => {
     fireEvent.click(img)
     expect(deleteTask).toHaveBeenCalledTimes(1)
     
-
-    
     render (
       <MemoryRouter>
         <Task 
@@ -73,8 +73,54 @@ describe('Task',  () => {
       </MemoryRouter>
      ) 
 
-     const emptyText = screen.getByText(emptyTask.text)
-     expect(emptyText).toBeInTheDocument()
+     const defaultDate = screen.getByText(emptyTask.date)
+     expect(defaultDate).toBeInTheDocument()
+  })
+
+  it('should be able to change task priority when task is double clicked', () => {
+    let changePriority = jest.fn()
+
+    let task = {
+      "text": "Dentist appointment",
+      "date": "July 17, 2021, 11:00 AM",
+      "priority": false,
+      "id": 1
+    }
+
+    render (
+      <MemoryRouter>
+        <Task 
+          task={task}
+          togglePriority={changePriority}
+        />
+      </MemoryRouter>
+     ) 
+
+     const text = screen.getByText(task.text)
+     expect(text).toBeInTheDocument()
+     const date = screen.getByText(task.date)
+     expect(date).toBeInTheDocument()
+     const img = screen.getByRole('img')
+     expect(img).toBeInTheDocument()
+     fireEvent.dblClick(text)
+     expect(changePriority).toHaveBeenCalledTimes(1)
+     
+     task = {
+      "text": "Dentist appointment",
+      "date": "July 17, 2021, 11:00 AM",
+      "priority": true,
+      "id": 1
+    }
+    render (
+      <MemoryRouter>
+        <Task 
+          task={task}
+          togglePriority={changePriority}
+        />
+      </MemoryRouter>
+     )
+     
+     expect(task.priority).toBe(true) 
   })
 
 })
