e166ff270d3dd314d6441f07b1baaadc6fd5fce7
Horacio Borrego <hborrego365@gmail.com>
2021-06-23 13:53:45 -0700

    Add test to confirm function call to submit a new task

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -74,11 +74,51 @@ describe('Form',  () => {
     const checkBox = screen.getByRole('checkbox')
     expect(checkBox).toBeInTheDocument()
     expect(checkBox.checked).toBe(false)
-    
+
     fireEvent.change(checkBox, {target:{checked: true}})
     expect(checkBox.checked).toBe(true)
 
   })
 
+  it ('should be able to submit a new task',  () => {
+    const addTask = jest.fn()
+    const taskToDo = 'Doctor\'s appointment'
+    const dateTimeValue = 'July 17, 2021, 11:00 AM'
+    render (
+      <MemoryRouter>
+        <Form 
+          addNewTask ={addTask}
+        />
+      </MemoryRouter>
+    )
+    
+    const task = screen.getByLabelText('Task')
+    expect(task).toBeInTheDocument()
+
+    const dateTime = screen.getByLabelText('Date & Time')
+    expect(dateTime).toBeInTheDocument()
+
+    const taskInput = screen.getByPlaceholderText('Add Task')
+    expect(taskInput).toBeInTheDocument()
+
+    const dateTimeInput = screen.getByPlaceholderText('Select a date & time')
+    expect(dateTimeInput).toBeInTheDocument()
+
+    const checkBox = screen.getByRole('checkbox')
+    expect(checkBox).toBeInTheDocument()
+
+    const button = screen.getByText('Submit this task')
+    expect(button).toBeInTheDocument()
+
+    fireEvent.change(taskInput, {target:{value: taskToDo}})
+    expect(taskInput.value).toBe(taskToDo)
+
+    fireEvent.change(dateTime, {target:{value: dateTimeValue}})
+    expect(dateTime.value).toBe(dateTimeValue)
+
+    fireEvent.click(button)
+    expect(addTask).toHaveBeenCalledTimes(1)
+
+  })
 
 })
