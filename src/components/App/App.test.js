68b3f8b805e53b71fcfcf75b290f458d79cbd7aa
Horacio Borrego <hborrego365@gmail.com>
2021-06-25 12:10:47 -0700

    Add integration test for removeTask api call

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import {  fireEvent, render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom'
 import App from '../App/App'
@@ -120,6 +120,7 @@ describe('App',  () => {
 
     const checkBox = screen.getByRole('checkbox')
     expect(checkBox).toBeInTheDocument()
+    expect(checkBox.checked).toBe(false)
 
     fireEvent.change(taskInput, {target:{value: newTask.text}})
     expect(taskInput.value).toBe(newTask.text)
@@ -167,7 +168,108 @@ describe('App',  () => {
     expect(date3).toBeInTheDocument()
   })
 
+  it('should be able to remove a task',  async () => {
+    let tasks = [{
+      "text": "Dentist appointment",
+      "date": "July 17, 2021, 11:00 AM",
+      "priority": false,
+      "id": 1
+      }, 
+      {
+      "text": "Buy groceries",
+      "date": "June 4, 2021, 11:00 AM",
+      "priority": false,
+      "id": 2
+      }
+    ]
 
+    await fetchTasks.mockResolvedValueOnce(tasks)
+
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )
+
+    await waitFor(() => expect(fetchTasks).toHaveBeenCalledTimes(1))
+
+    let text1 = screen.getByText(tasks[0].text)
+    let date1 = screen.getByText(tasks[0].date)
+    expect(text1).toBeInTheDocument()
+    expect(date1).toBeInTheDocument()
+
+    let text2 = screen.getByText(tasks[1].text)
+    let date2 = screen.getByText(tasks[1].date)
+    expect(text2).toBeInTheDocument()
+    expect(date2).toBeInTheDocument()
+
+    let deleteIconTask2 = screen.getAllByRole('img')
+    expect(deleteIconTask2[1]).toBeInTheDocument()
+
+    fireEvent.click(deleteIconTask2[1])
+    await removeTask.mockResolvedValueOnce(tasks[1].id)  
+    await waitFor(() => expect(removeTask).toHaveBeenCalledTimes(1))
+    
+    tasks = [{
+      "text": "Dentist appointment",
+      "date": "July 17, 2021, 11:00 AM",
+      "priority": false,
+      "id": 1
+      }
+    ]
+
+    await fetchTasks.mockResolvedValueOnce(tasks)
+    await waitFor(() => expect(fetchTasks).toHaveBeenCalledTimes(1))
+
+    expect(tasks).toHaveLength(1)
+  })
+
+  // it('should be able to change the priority of a task', async () =>{
+  //   let tasks = [{
+  //     "text": "Dentist appointment",
+  //     "date": "July 17, 2021, 11:00 AM",
+  //     "priority": false,
+  //     "id": 1
+  //     }
+  //   ]
+
+  //   let updatedTask = {
+  //     "text": "Dentist appointment",
+  //     "date": "July 17, 2021, 11:00 AM",
+  //     "priority": true,
+  //     "id": 1
+  //     }
+
+  //   await fetchTasks.mockResolvedValueOnce(tasks)
+
+  //   render(
+  //     <MemoryRouter>
+  //       <App />
+  //     </MemoryRouter>
+  //   )
+
+  //   await waitFor(() => expect(fetchTasks).toHaveBeenCalledTimes(1))
+
+  //   let text1 = screen.getByText(tasks[0].text)
+  //   let date1 = screen.getByText(tasks[0].date)
+    
+  //   expect(text1).toBeInTheDocument()
+  //   expect(date1).toBeInTheDocument()
+  //   expect(tasks[0].priority).toBe(false)
+    
+  //   fireEvent.dblClick(text1)
+  //   await waitFor(() => expect(fetchTask).toHaveBeenCalledTimes(1))
+  //   await fetchTask.mockResolvedValueOnce(tasks[0])
+    
+
+
+
+    // await changePriority.mockResolvedValueOnce(tasks[0].id, updatedTask)
+
+
+    // await waitFor(() => expect(changePriority).toHaveBeenCalledTimes(1))
+    // expect(priority1).toBe(true)
+  // })
 
 })
 
